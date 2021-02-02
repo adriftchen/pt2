d7cb9f28ad45fdaf5891f79756b03c1dfcdc1c5b
linkzepp <adriftchen@gmail.com>
2021-02-02 16:33:44 +0800

    完成會員登入檢查的功能

--- front/login.php
+++ front/login.php
@@ -4,17 +4,39 @@
       <table>
         <tr>
           <td>帳號</td>
-          <td><input type="text" name="" id=""></td>
+          <td><input type="text" name="acc" id="acc"></td>
         </tr>
         <tr>
           <td>密碼</td>
-          <td><input type="password"></td>
+          <td><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
-          <td><input type="submit" value="登入"><input type="reset" value="清除"></td>
+          <td><input type="button" value="登入" onclick="login()"><input type="reset" value="清除"></td>
           <td><a href="?do=forget">忘記密碼</a>|<a href="?do=reg">尚未註冊</a></td>
         </tr>
       </table>
 
   </fieldset>
 </form>
+<script>
+  function login(){
+    let acc=$("#acc").val()
+    let pw=$("#pw").val()
+
+    $.post("api/ckacc.php",{acc},function(res){
+      if(res=='1'){
+        $.post("api/ckacc.php",{acc,pw},function(r){
+          if(r=='1'){
+            location.href="index.php";
+          }else{
+            alert("密碼錯誤");
+            $("#acc,#pw").val("");
+          }
+        })
+      }else{
+        alert("查無帳號");
+        $("#acc,#pw").val("");
+      }
+    })
+  }
+</script>
