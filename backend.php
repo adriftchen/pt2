ca3b22f562dfe707d380f5dd2cddfe88eda341a3
linkzepp <adriftchen@gmail.com>
2021-02-02 15:22:34 +0800

    DW完成主畫面

--- backend.php
+++ backend.php
@@ -30,23 +30,11 @@
                	                 </div>
             <div class="hal" id="main">
             	<div>
-					<marquee style="width:70%">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地。詳見最新文章</marquee>
             		
                 	<span style="width:18%; display:inline-block;">
                     	                    	<a href="?do=login">會員登入</a>
                     	                    </span>
                     	<div class="">
-<?php
-$do=(isset($_GET['do']))?$_GET['do']:'main';
-$file="front/".$do.".php";
-// echo $do;
-// echo $file;
-if(file_exists($file)){
-	include_once $file;
-}else{
-	include_once "front/main.php";
-}
-?>
                 		                        </div>
                 </div>
             </div>
---
+++ front/po.php
@@ -0,0 +1,20 @@
+<script src="SpryAssets/SpryTabbedPanels.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryTabbedPanels.css" rel="stylesheet" type="text/css" />
+<div id="TabbedPanels1" class="TabbedPanels">
+  <ul class="TabbedPanelsTabGroup">
+    <li class="TabbedPanelsTab" tabindex="0">健康新知</li>
+    <li class="TabbedPanelsTab" tabindex="0">菸害防治</li>
+    <li class="TabbedPanelsTab" tabindex="0">癌症防治</li>
+<li class="TabbedPanelsTab" tabindex="0">慢性病防治<br />
+</li>
+</ul>
+  <div class="TabbedPanelsContentGroup">
+    <div class="TabbedPanelsContent">內容 1</div>
+    <div class="TabbedPanelsContent">內容 2</div>
+    <div class="TabbedPanelsContent">內容 3</div>
+<div class="TabbedPanelsContent">內容 4</div>
+</div>
+</div>
+<script type="text/javascript">
+var TabbedPanels1 = new Spry.Widget.TabbedPanels("TabbedPanels1");
+</script>
