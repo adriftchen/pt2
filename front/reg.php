dbbc1b6674a0333f29bd5ee64686c0ae4d4dfcd5
linkzepp <adriftchen@gmail.com>
2021-02-02 15:56:28 +0800

    建置會員註冊/登入/忘記密碼的頁面

---
+++ front/reg.php
@@ -0,0 +1,29 @@
+<form action="">
+  <fieldset>
+    <table>
+      <div style="color:red">*請設定您要註冊的帳號與密碼(最長12字元)</div>
+      <table>
+        <tr>
+          <td>Step 1:登入帳號</td>
+          <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+          <td>Step 2:登入密碼</td>
+          <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+          <td>Step 3:再次確認密碼</td>
+          <td><input type="password" name="pw2" id="pw2"></td>
+        </tr>
+        <tr>
+          <td>Step 4:信箱(忘記密碼時使用)</td>
+          <td><input type="text" name="email" id="email"></td>
+        </tr>
+        <tr>
+          <td><input type="submit" value="註冊"><input type="reset" value="清除"></td>
+          <td></td>
+        </tr>
+      </table>
+    </table>
+  </fieldset>
+</form>
